502425c843cbe9a37a328b21f27a97423151d570
cloner02 <cloner17@gmail.com>
2024-03-05 19:43:45 +0100

    remove comments

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,8 @@
-// import { ElementJsonFormatError, PropertyNotFoundError } from './../exceptions/index'
+import { ElementJsonFormatError, PropertyNotFoundError } from './../exceptions/index'
 
-/* export function getPropertyValueFromJson (propertyName: string, json: string): any {
-  const parsedJson: Record<string, any> = JSON.parse(json)
+// eslint-disable-next-line no-extend-native
+export default String.prototype.getPropertyValueFromJson = function (propertyName: string): any {
+  const parsedJson: Record<string, any> = JSON.parse(this.toString())
   const keys = Object.keys(parsedJson)
 
   if (keys.length !== 1) {
@@ -12,24 +13,6 @@
 
   if (!Object.prototype.hasOwnProperty.call(infoOfElement, propertyName)) {
     throw new PropertyNotFoundError(propertyName)
-  }
-  return infoOfElement[propertyName]
-} */
-
-// eslint-disable-next-line no-extend-native
-export default String.prototype.getPropertyValueFromJson = function (propertyName: string): any {
-  console.log('this', this)
-  const parsedJson: Record<string, any> = JSON.parse(this.toString())
-  const keys = Object.keys(parsedJson)
-
-  if (keys.length !== 1) {
-    throw new Error('JSON is empty or is not a valid JSON object for a element of the form.')
-  }
-  const key = keys[0]
-  const infoOfElement = parsedJson[key] ?? {}
-
-  if (!Object.prototype.hasOwnProperty.call(infoOfElement, propertyName)) {
-    throw new Error(`Property ${propertyName} not found in JSON`)
   }
   return infoOfElement[propertyName]
 }
