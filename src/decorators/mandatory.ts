d7c46cb63334c8fc4d7e5f9c6e637570e694db64
cloner02 <cloner17@gmail.com>
2024-04-19 14:02:08 +0200

    fix and new example

--- src/decorators/mandatory.ts
+++ src/decorators/mandatory.ts
@@ -1,4 +1,3 @@
-import { CInput } from '../components/cinput/cinput'
 import { type CDynamicBase } from '../components/cdynamicbase/cdynamicbase'
 
 export function mandatoryMsg (getElement: (instance: any) => CDynamicBase): MethodDecorator {
@@ -8,8 +7,8 @@ export function mandatoryMsg (getElement: (instance: any) => CDynamicBase): Meth
     descriptor.value = function (...args: any[]) {
       const element = getElement(this)
       let msgError: string | null = null
-      if (element instanceof CInput) {
-        msgError = (element.mandatory && (element.value.trim() === '' || element.value.trim() === null || element.value.trim() === undefined)) ? `the ${element.label} field is empty` : null
+      if ('mandatory' in element) {
+        msgError = ((Boolean(element.mandatory)) && (element.value.trim() === '' || element.value.trim() === null || element.value.trim() === undefined)) ? `the ${element.label} field is empty` : null
       }
       args[0] = (args[0] === undefined && msgError !== null) ? msgError : args[0]
       return originaMethod.apply(this, args)
